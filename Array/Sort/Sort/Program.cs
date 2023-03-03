69a95256aa056e4752da3ed650881dbb33dddc9e
Matteo <matteo.fuso@iisviolamarchesini.edu.it>
2023-03-03 15:23:47 +0100

    Sort Algo

---
+++ Array/Sort/Sort/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Sort
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long start, end;
+            int[] template = new int[10] { 1, 96, 365, 45, 3, -96, -78, 35, 45, 0 };
+            int[] toSort = new int[template.Length];
+            int options = 2, input, temp;
+            string arr = "[ " + template[0] + ", ";
+            for (int i = 1; i < template.Length - 1; i++)
+            {
+                arr += template[i] + ", ";
+            }
+            arr += template[template.Length - 1] + " ]";
+            do
+            {
+                Console.Clear();
+                template.CopyTo(toSort, 0);
+                Console.WriteLine("Array da ordinare: {0}", arr);
+                Console.WriteLine("Ecco i metodi di sort a disposizione: ");
+                Console.WriteLine("1. Bubble Sort");
+                Console.WriteLine("2. Esci");
+                Console.Write("Inserisci la tua scelta: ");
+                input = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine();
+                while (input < 0 || input > options)
+                {
+                    Console.WriteLine("Scelta non valida. Reinserisci.");
+                    input = Convert.ToInt32(Console.ReadLine());
+                }
+                if (input != options)
+                {
+                    switch (input)
+                    {
+                        case 1:
+                            start = Stopwatch.GetTimestamp();
+                            for (int condizioni = toSort.Length - 1; condizioni > 0; condizioni--)
+                            {
+                                for (int i = 0; i < condizioni; i++)
+                                {
+                                    if (toSort[i] > toSort[i + 1])
+                                    {
+                                        temp = toSort[i];
+                                        toSort[i] = toSort[i + 1];
+                                        toSort[i + 1] = temp;
+                                    }
+                                }
+                            }
+                            end = Stopwatch.GetTimestamp();
+                            Console.WriteLine("L'array ordinato è:");
+                            for (int i = 0; i < toSort.Length; i++)
+                            {
+                                Console.WriteLine(toSort[i]);
+                            }
+                            Console.WriteLine("\n\rL'esecuzione ha impiegato {0} ticks.", end - start);
+                            break;
+                    }
+                    Console.ReadLine();
+                }
+            } while (options != 2);
+        }
+    }
+}
